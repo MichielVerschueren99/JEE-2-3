68ef9404ca9a60e73ff1aedb2c8ecfd18ced74a9
osboxes <osboxes@osboxes>
2020-11-15 14:31:16 -0500

    eerste jpql in mqnqger

--- CarRental-ejb/src/java/session/ManagerSession.java
+++ CarRental-ejb/src/java/session/ManagerSession.java
@@ -21,28 +21,39 @@ public class ManagerSession implements ManagerSessionRemote {
     
     @Override
     public Set<CarType> getCarTypes(String company) {
-        try {
-            CarRentalCompany requestedCompany = em.find(CarRentalCompany.class, company);
-            return new HashSet<CarType>(requestedCompany.getAllTypes());
-        } catch (IllegalArgumentException ex) {
-            Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
+        List<CarType> result = em.createQuery("SELECT cc.carTypes FROM CarRentalCompany cc WHERE cc.name=:givenCompany")
+                .setParameter("givenCompany", company)
+                .getResultList();
+        return  new HashSet<>(result);
+        //try {
+        //    CarRentalCompany requestedCompany = em.find(CarRentalCompany.class, company);
+        //    return new HashSet<CarType>(requestedCompany.getAllTypes());
+        //} catch (IllegalArgumentException ex) {
+        //    Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
+        //    return null;
+        //}
     }
 
     @Override
     public Set<Integer> getCarIds(String company, String type) {
-        Set<Integer> out = new HashSet<Integer>();
-        try {
-            CarRentalCompany requestedCompany = em.find(CarRentalCompany.class, company);
-            for(Car c: requestedCompany.getCars(type)){
-                out.add(c.getId());
-            }
-        } catch (IllegalArgumentException ex) {
-            Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-        return out;
+        List<Integer> result = em.createQuery("SELECT c.id FROM Car c "
+                + "WHERE EXISTS (SELECT cc FROM CarRentalCompany WHERE cc.name = :givenCompany AND c IN(cc.cars)) "
+                    + "AND EXISTS (SELECT ct FROM CarType WHERE ct.type = :givenType AND c.type=ct")
+                        .setParameter("givenType", type)
+                        .setParameter("givencompany", company)
+                        .getResultList();
+        return new HashSet<>(result);
+        //Set<Integer> out = new HashSet<Integer>();
+        //try {
+        //    CarRentalCompany requestedCompany = em.find(CarRentalCompany.class, company);
+        //    for(Car c: requestedCompany.getCars(type)){
+        //        out.add(c.getId());
+        //   }
+        //} catch (IllegalArgumentException ex) {
+        //    Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
+        //    return null;
+        //}
+        //return out;
     }
 
     @Override
@@ -93,11 +104,18 @@ public class ManagerSession implements ManagerSessionRemote {
 
     @Override
     public int getNumberOfReservationsBy(String clientName) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Object result = em.createQuery("SELECT COUNT(r) FROM Reservation r WHERE r.carRenter = :givenName ")
+                .setParameter("givenName", clientName)
+                .getSingleResult();
+        return ((Long) result).intValue();
     }
 
     @Override
     public int getNumberOfReservationsByCarType(String carRentalName, String carType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Object result = em.createQuery("SELECT COUNT(r) FROM Reservation r WHERE r.rentalCompany= :givenCompany AND r.carType = :givenType")
+                .setParameter("givenCompany", carRentalName)
+                .setParameter("givenType", carType)
+                .getSingleResult();
+        return ((Long) result).intValue();
     }
 }
